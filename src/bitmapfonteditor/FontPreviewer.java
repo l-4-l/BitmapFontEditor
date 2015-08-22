1cb50166a326250595a7fa3f316e86da9667c62b
l-4-l <lordakryl@gmail.com>
2015-08-22 14:10:29 +0300

    Netbeans project files added

---
+++ src/bitmapfonteditor/FontPreviewer.java
@@ -0,0 +1,32 @@
+package bitmapfonteditor;
+
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.JComponent;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ *
+ * @author l4l
+ */
+public class FontPreviewer extends JComponent implements AdjustmentListener, ChangeListener, ItemListener {
+
+    @Override
+    public void adjustmentValueChanged(AdjustmentEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
